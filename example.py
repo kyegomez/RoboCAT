c71eb373403262d308315e8d3ff1c7038a839445
Kye <kye@apacmediasolutions.com>
2023-09-04 11:54:13 -0400

    forward

--- example.py
+++ example.py
@@ -3,12 +3,11 @@ from robocat.model import RoboCat
 
 model = RoboCat()
 
-
 video = torch.randn(2, 3, 6, 224, 224)
 instructions = [
     "bring me that apple"
 ]
 
-result = model(video, instructions)
+result = model.forward(video, instructions)
 print(result)
 
